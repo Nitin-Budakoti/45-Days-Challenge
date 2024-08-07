8b0ae7ab2d359938649326ae7ac4130bdd5f1a46
Nitin-Budakoti <151420006+Nitin-Budakoti@users.noreply.github.com>
2024-08-07 18:03:26 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (26.12%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sorting( vector<int>&nums){
+        int n = nums.size();
+        for(int i  = 0 ; i< n-1;i++){
+            for(int j = i+1;j<n;j++){
+                if(nums[i]>nums[j]){
+                    swap(nums[i],nums[j]);
+                    }
+            }
+        }
+    }
+    void sortColors(vector<int>& nums) {
+        sorting(nums);
+    }
+};
