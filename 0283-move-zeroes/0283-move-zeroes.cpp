7de0b6ce67c99d7308fc22efb462d3f020f5867a
Nitin-Budakoti <151420006+Nitin-Budakoti@users.noreply.github.com>
2024-08-07 21:16:39 +0530

    Time: 20 ms (26.71%), Space: 22.8 MB (5.05%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zeros =0;
-        for(int i = 0 ; i<nums.size();i++){
-            if(nums[i]==0) zeros++;
-    }
         vector<int>arr;
         for(int i = 0 ; i<nums.size();i++){
             if(nums[i]!=0)
