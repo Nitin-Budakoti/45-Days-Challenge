2ec700992a55cb8d902898f3f230a97e30af0eb4
Nitin-Budakoti <151420006+Nitin-Budakoti@users.noreply.github.com>
2024-08-09 10:35:23 +0530

    Time: 178 ms (6.51%), Space: 29.7 MB (19.70%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -37,10 +37,39 @@ public:
         dp[i][M][alice] = ans;
         return ans;
   }
+    
+    int solveTab(vector<int>&piles){
+    vector<vector<vector<int>>>dp(piles.size()+1,vector<vector<int>>(piles.size()+1,vector<int>(2,-0)));
+        int n = piles.size();
+        for(int i = n-1 ; i>=0 ; --i){
+            for(int M = 1 ; M < piles.size();++M ) {
+                for(int alice = 0 ;alice<2 ;alice++){
+                    int ans =alice? INT_MIN:INT_MAX;
+                      int total = 0;
+                      for(int X = 1 ;X<=2*M;X++){
+                         if((i+X-1)>= piles.size()) break;
+                            total += piles[i+X-1];
+                         if(alice){
+                            ans = max(ans,total +dp[i+X][max(X,M)][0]);
+                          }
+                         else{
+                                ans = min(ans , dp[i+X][max(X,M)][1]);          
+                          }
+                        dp[i][M][alice] = ans;
+                    }
+                    
+                }            
+            }
+                                     
+           }
+            return dp[0][1][1];
+        }                             
     int stoneGameII(vector<int>& piles) {
         //int ans = solveReccursive(piles,0,1,true);
-        vector<vector<vector<int>>>dp(piles.size()+1,vector<vector<int>>(piles.size()+1,vector<int>(2,-1)));
-        int ans = solveMemo(piles,0,1,true,dp);
+       // vector<vector<vector<int>>>dp(piles.size()+1,vector<vector<int>>(piles.size()+1,vector<int>(2,-1)));
+        //int ans = solveMemo(piles,0,1,true,dp);
+        if(piles.size()==1) return piles[0];
+        int ans  =  solveTab(piles);
         return ans;
         
     }
