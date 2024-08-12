52beef7aefb75fca2a857df407494173d160e830
Nitin-Budakoti <151420006+Nitin-Budakoti@users.noreply.github.com>
2024-08-12 21:13:07 +0530

    Time: 52 ms (62.11%), Space: 13.8 MB (17.05%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -30,9 +30,27 @@ public:
         dp[n] =ans;
         return ans;
     }
+    int solveTab(int n ){
+    vector<int>dp(n+1 ,0);
+        dp[0] =1;
+        for(int k =1 ;k<=n;k++){
+            int s = sqrt(k);
+            int ans = INT_MAX;
+            for(int i = 1; i<=s;i++)
+                {
+                    int ps = i*i;
+                    ans =  min(ans ,1+ dp[k-ps]);
+                }
+            dp[k] =ans; 
+        }
+        
+        return dp[n];
+    }
+    
     int numSquares(int n) {
         vector<int>dp(n+1 ,-1);
-        int ans = solvememo(n ,dp);
+        //int ans = solvememo(n ,dp);
+        int ans = solveTab(n);
         return ans-1;
     }
 };
