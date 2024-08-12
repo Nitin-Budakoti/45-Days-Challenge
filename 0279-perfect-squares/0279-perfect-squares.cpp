09fd80b2859b8b7300d11fba46bb7f84e77aa51a
Nitin-Budakoti <151420006+Nitin-Budakoti@users.noreply.github.com>
2024-08-12 21:03:23 +0530

    Time: 88 ms (36.36%), Space: 11.9 MB (27.69%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,59 +1,38 @@
 class Solution {
 public:
-    int solveReccursive(int n){
-    //base case 
+    int solve(int n )
+    {
+     //base case
         if(n==0) return 1;
         if(n<0) return 0;
-        int end = sqrt(n);
-        int ans= INT_MAX;
-        for( int i = 1 ; i<=end;i++ ){
-            int number = i*i;
-            ans = min(ans ,1+ solveReccursive(n-number));
-    }
-        return ans;
-    } 
-    
-    int solveMemo(int n ,vector<int>&dp){
-     //base case 
-        if(n==0) return 1;
-        if(n<0) return 0;
-        //check condition for dp
-        if(dp[n]!=-1) return dp[n];
-        int end = sqrt(n);
-        int ans= INT_MAX;
-        for( int i = 1 ; i<=end;i++ ){
-            int number = i*i;
-            ans = min(ans ,1+ solveMemo(n-number,dp));
-    }
-        dp[n]= ans;
-        return ans;
-    }
-    
-    
-    
-    int solveTab(int n ){
-        vector<int>dp(n+1,0);
-        dp[0] = 1;
-       
-        for(int j = 1 ; j<=n;j++){           
-        int ans= INT_MAX;
-        int end = sqrt(j);
-        for( int i = 1 ; i<=end;i++ ){
-            int number = i*i;
-            ans = min(ans ,1+ dp[j-number]);
-            }
-        dp[j]= ans;
+        int s = sqrt(n);
+        int ans = INT_MAX;
+        for(int i = 1; i<=s;i++)
+        {
+            int ps = i*i;
+            ans =  min(ans ,1+ solve(n-ps));
         }
-    return dp[n];    
+        return ans;
     }
-    
-    
-    
+    int solvememo(int n ,  vector<int>&dp)
+    {
+     //base case
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int s = sqrt(n);
+        int ans = INT_MAX;
+        for(int i = 1; i<=s;i++)
+        {
+            int ps = i*i;
+            ans =  min(ans ,1+ solvememo(n-ps,dp));
+        }
+        dp[n] =ans;
+        return ans;
+    }
     int numSquares(int n) {
-        //int ans = solveReccursive(n);
-        // vector<int>dp(n+1,-1);
-        // int ans = solveMemo(n,dp);
-        int ans = solveTab(n);
+        vector<int>dp(n+1 ,-1);
+        int ans = solvememo(n ,dp);
         return ans-1;
     }
 };
