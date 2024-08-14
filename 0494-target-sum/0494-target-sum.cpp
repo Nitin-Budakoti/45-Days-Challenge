a151a0b65dfaa8afb374098ae866dfc2393f0cba
Nitin-Budakoti <151420006+Nitin-Budakoti@users.noreply.github.com>
2024-08-14 17:39:30 +0530

    Time: 261 ms (31.33%), Space: 62 MB (9.80%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -8,8 +8,19 @@ public:
         int minus = solveReccursive(nums,target-nums[start],start+1);
         return plus +minus; 
     }
+     int solveMemo(vector<int>&nums, int target,int start, map<pair<int,int>,int>&dp){
+        //base case
+        if(start ==nums.size())
+            return target==0?1:0;
+         if(dp.find({start,target}) != dp.end() ) return dp[{start,target}];
+
+        int plus = solveMemo(nums,target+nums[start],start+1,dp);
+        int minus = solveMemo(nums,target-nums[start],start+1,dp);
+        return dp[{start,target}] = plus+minus;
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int ans = solveReccursive(nums,target,0);
+        map<pair<int,int> ,int>dp;
+        int ans = solveMemo(nums,target,0,dp);
         return ans;
     }
 };
