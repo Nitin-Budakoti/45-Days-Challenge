65506e4bb7c607a17d05f8ed5d5b498adaec7876
Nitin-Budakoti <151420006+Nitin-Budakoti@users.noreply.github.com>
2024-08-18 09:29:50 +0530

    Time: 22 ms (51.01%), Space: 16.9 MB (39.31%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -37,10 +37,38 @@ public:
         dp[i][j] = cost;
         return cost;
     }
+    int solveTab(string& s1, string& s2){
+       vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,0)); 
+        
+        //base cases
+        for(int i =s1.size()-1;i>=0;i--){
+            dp[i][s2.size()] =s1[i]+dp[i+1][s2.size()];
+        }
+        for(int i =s2.size()-1;i>=0;i--){
+            dp[s1.size()][i] =s2[i]+ dp[s1.size()][i+1];
+        }
+        //normal converting reccursion calls to loop
+        int cost = 0;
+        for(int i = s1.size()-1 ;i>=0;--i){
+            for(int j = s2.size()-1;j>=0;--j){
+                if(s1[i]==s2[j])
+                   cost = solveMemo(s1,s2,i+1 ,j+1,dp);
+                else{
+                    int cost1 = s1[i]+dp[i+1][j];
+                    int cost2 = s2[j] +dp[i][j+1];
+                    cost = min(cost1, cost2);
+                }
+                dp[i][j] = cost;
+            }
+        }
+        return dp[0][0];
+    }
+    
     int minimumDeleteSum(string s1, string s2) {
         //int ans = solveReccursive(s1,s2,0,0);
-        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
-        int ans = solveMemo(s1,s2,0,0,dp);
+        // vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
+        // int ans = solveMemo(s1,s2,0,0,dp
+        int ans = solveTab(s1,s2);
         return ans;
         
     }
