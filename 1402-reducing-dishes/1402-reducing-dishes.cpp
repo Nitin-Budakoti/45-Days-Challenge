c67e36b27f47ad0111ce23ae5c29a5251ded10fd
Nitin-Budakoti <151420006+Nitin-Budakoti@users.noreply.github.com>
2024-08-22 17:59:01 +0530

    Time: 19 ms (39.25%), Space: 36 MB (7.31%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -15,10 +15,21 @@ public:
         int exclude = solveMemo(arr,i+1,t,dp);
         return dp[i][t] =max(include,exclude);
     }
+    int solveTab(vector<int>&arr){
+        vector<vector<int>>dp(arr.size()+2 ,vector<int>(arr.size()+2,0));
+        for(int i = arr.size()-1;i>=0 ;--i){
+            for(int t = arr.size();t>=0;--t){
+                 int include = arr[i]*t + dp[i+1][t+1];
+                 int exclude = dp[i+1][t];
+                     dp[i][t] =max(include,exclude);
+            }
+        }
+        return dp[0][1];
+    }
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin() ,satisfaction.end());
         vector<vector<int>>dp(satisfaction.size()+1 ,vector<int>(satisfaction.size()+1,-1));
-        int ans = solveMemo(satisfaction,0,1,dp);
+        int ans = solveTab(satisfaction);
         return ans;
     }
 };
